6403e3d03f614593fb1958b179af9ed2f00ffd4d
Tawan-ok <saiordday1@gmail.com>
2025-02-26 20:32:43 +0700

    first commit

---
+++ src/main/java/com/example/High_Performance/Inventory/Management/System/HighPerformanceInventoryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.High_Performance.Inventory.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighPerformanceInventoryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighPerformanceInventoryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/High_Performance/Inventory/Management/System/HighPerformanceInventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.High_Performance.Inventory.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighPerformanceInventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
